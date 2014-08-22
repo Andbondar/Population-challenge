e14907b83cf7a0a5b50d13186160a7f5da1a3000
Mike Krichevsky <mkrichevsky@connecture.com>
2014-08-22 11:03:08 +0300

    Refactor changes

--- population.js
+++ population.js
@@ -1,16 +1,16 @@
 /**
  * Created by Andrew on 20.08.2014.
  */
-$(document).ready(function () {
-    $('.tooltip').tooltipster({     //activate tooltips
+$(document).ready(function() {
+
+    $('.tooltip').tooltipster({ //http://iamceege.github.io/tooltipster/
         theme: 'tooltipster-light',
         position: 'bottom-right',
         maxWidth: 120
     });
+
     setInterval(unemployedGenerator, 10000);
-    setInterval(totalPopulation, 10000);//updates population count
-    setInterval(foodProduction,10000); //food production and famine
-    setInterval(updateCount, 100);
+    setInterval(foodProduction, 10000);
 
     var population = {
         unemployed: 2,
@@ -24,7 +24,7 @@ $(document).ready(function () {
         money: 20,
         food: 30,
         reputation: 0,
-        score:0
+        score: 0
     };
 
     var research = {
@@ -32,14 +32,17 @@ $(document).ready(function () {
         farmExperience: 0,
         scienceLevel: 0,
         scienceExperience: 0
-
     };
+
+    updateCount();
 
     function updateCount() {
         $('#unemployed_count').text(population.unemployed);
         $('#farmers').text(population.farmers);
         $('#scientists').text(population.scientists);
         $('#government').text(population.government);
+        population.total = population.farmers + population.scientists + population.government + population.unemployed;
+        $('#population_count').text(population.total);
         $('#money').text(resources.money);
         $('#food').text(resources.food);
         $('#reputation').text(resources.reputation);
@@ -51,21 +54,21 @@ $(document).ready(function () {
     }
 
     function sendMessage(text) {
-        $('p').filter('.message').remove(); //all messages are cleared if something was displayed before
-        $('body').append('<p class="message">' + text.toString(10) + '</p>');
-        $('p').filter('.message').delay(3000).fadeOut('slow'); //query should be reused cause it received new object
+        $('#messages').text(text).show().delay(3000).fadeOut('slow');
     }
 
     function unemployedGenerator() {
         population.unemployed += 1;
+        updateCount();
     }
 
-    $("#farm_button").click(function () { //if unemployed citizen and 5 food are available - convert unemployed to farmer
+    $("#farm_button").click(function() {
         if (population.unemployed >= 1) {
             if (resources.food >= 5) {
-                population.unemployed -= 1; //decrease unemployed count by 1
-                population.farmers += 1; //increase farmers count by 1
-                resources.food -= 5; //decrease food count by 5
+                population.unemployed -= 1;
+                population.farmers += 1;
+                resources.food -= 5;
+                updateCount();
             } else {
                 sendMessage(errorMessages.notEnoughFood);
             }
@@ -74,7 +77,7 @@ $(document).ready(function () {
         }
     });
 
-    $('#science_button').click(function () {
+    $('#science_button').click(function() {
         if (population.unemployed >= 1) {
             if (resources.food >= 10) {
                 if (resources.money >= 10) {
@@ -82,6 +85,7 @@ $(document).ready(function () {
                     population.scientists += 1;
                     resources.food -= 10;
                     resources.money -= 10;
+                    updateCount();
                 } else {
                     sendMessage(errorMessages.notEnoughMoney);
                 }
@@ -93,21 +97,19 @@ $(document).ready(function () {
         }
     });
 
-    function totalPopulation() {
-        population.total = population.farmers + population.scientists + population.government + population.unemployed;
-    }
-
-    function foodProduction(){
-        resources.food = resources.food + population.farmers*3 - population.total;
-        if (resources.food<0){
-            population.unemployed = parseInt(Math.floor(population.unemployed*0.5)); //50% of unemployed died. 50% left
-            population.scientists = parseInt(Math.floor(population.scientists*0.8)); //20% of scientists died, 80% left
+    function foodProduction() {
+        resources.food = resources.food + population.farmers * 3 - population.total;
+        if (resources.food < 0) {
+            population.unemployed = parseInt(Math.floor(population.unemployed * 0.5)); //50% of unemployed died. 50% left
+            population.scientists = parseInt(Math.floor(population.scientists * 0.8)); //20% of scientists died, 80% left
             resources.food = 0;
-            sendMessage(errorMessages.famine);
+            updateCount();
+            sendMessage(eventMessages.famine);
+            return;
+        } else if (population.total > resources.food) {
+            sendMessage(warningMessages.famineDanger);
         }
-        else if (population.total>resources.food){
-            sendMessage(errorMessages.famineDanger);
-        }
+        updateCount();
     }
 
 });
--- properties.js
+++ properties.js
@@ -1,7 +1,13 @@
 errorMessages = {
     notEnoughFood: "Not enough food. Train more farmers.",
     notEnoughUnemployed: "There are currently no unemployed citizens.",
-    notEnoughMoney: "Not enough money.",
-    famine: "Not enough Food. Some scientists and unemployed citizens have died.",
+    notEnoughMoney: "Not enough money."
+};
+
+warningMessages = {
     famineDanger: "Food has almost ended. There is danger of famine."
+};
+
+eventMessages = {
+    famine: "Not enough Food. Some scientists and unemployed citizens have died."
 };
